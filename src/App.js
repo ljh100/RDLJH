fcac4b6938eca0d242b0d2bc9b2515aacd0d0ea7
xofserve <samgain69@gmail.com>
2019-02-22 11:12:05 -0500

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers=[
+{
+  'id':1,
+  'image':'http://placeimg.com/64/64/1',
+  'name':'이준희11s',
+  'birthday':'19601015',
+  'address':'11134d.. thornhill wood'
+},
+{
+  'id':2,
+  'image':'http://placeimg.com/64/64/2',
+  'name':'이준희22s',
+  'birthday':'19601015',
+  'address':'22234d.. thornhill wood'
+},
+{
+  'id':3,
+  'image':'http://placeimg.com/64/64/3',
+  'name':'이준희33s',
+  'birthday':'19601015',
+  'address':'33334d.. thornhill wood'
+}
+]
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className="gray-background">
+        {/* <Customer 
+        id={customer[0].id}
+        image={customer[0].image}
+        name={customer[0].name}
+        birthday={customer[0].birthday}
+        address={customer[0].address}/>
+        <Customer 
+        id={customer[1].id}
+        image={customer[1].image}
+        name={customer[1].name}
+        birthday={customer[1].birthday}
+        address={customer[1].address}/> 
+        */}
+        {
+          customers.map(c => {
+            return(
+                <Customer
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                address={c.address}
+                />
+            );
+          }
+
+          )
+        }
+      </div>  
+      
     );
   }
 }
---
+++ src/components/Customer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class Customer extends React.Component{
+    render(){
+        return(
+        <div>            
+            <CustomerProfile id={this.props.id} name={this.props.name} image={this.props.image}/>
+            <CusstomerInfo birthday={this.props.birthday} address={this.props.address} />
+        </div>
+        )
+    }
+}
+class CustomerProfile extends React.Component{
+    render(){
+        return(
+
+            <div>
+                <img src={this.props.image} alt="profile" />
+                <h2>{this.props.name}({this.props.id})</h2>
+            </div>
+
+        );
+    }
+}
+class CusstomerInfo extends React.Component{
+    render(){
+        return(
+            <div>
+                <p>{this.props.birthday}</p>
+                <p>{this.props.address}</p>
+            </div>
+        )
+    }
+}
+export default Customer;
