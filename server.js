ba9f6c5be042494c09195b7f060c8cab5ba8d78f
xofserve <samgain69@gmail.com>
2019-02-23 14:44:18 -0500

    Connection of AWS RDS in Reat

--- server.js
+++ server.js
@@ -1,41 +1,33 @@
+const fs=require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app=express();
 const port = process.env.PORT || 5000;
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/customers',(req,res)=>{
-    
-    //res.send({message: 'Helllo '});
-    res.send(
+const data=fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');//mysql lib called
+const connection = mysql.createConnection({
+    host:conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+connection.connect();
 
-        [{
-            "id": 1,
-            "image": "http://placeimg.com/64/64/1",
-            "name": "이준희11s",
-            "birthday": "19601015",
-            "address": "99934d.. thornhill wood"
-        }, {
-            "id": 2,
-            "image": "http://placeimg.com/64/64/2",
-            "name": "이준희22s",
-            "birthday": "19601015",
-            "address": "22234d.. thornhill wood"
-        }, {
-            "id": 3,
-            "image": "http://placeimg.com/64/64/3",
-            "name": "이준희44s",
-            "birthday": "19601015",
-            "address": "9994d.. thornhill wood"
-        }, {
-            "id": 4,
-            "image": "http://placeimg.com/64/64/3",
-            "name": "이준희33s",
-            "birthday": "19601015",
-            "address": "4545d.. thornhill wood"
-        }]
+app.get('/api/customers',(req,res)=>{
+
+    //res.send({message: 'Helllo '});
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err,rows,fields) =>{           
+        res.send(rows);
+        }
     );
 });
 
